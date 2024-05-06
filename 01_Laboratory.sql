4ada1c1e221a4839229de944111908d4b3c97100
Andrey Garcia <andreygarcia508@gmail.com>
2024-05-06 08:53:31 +0800

    first commit

---
+++ 01_Laboratory.sql
@@ -0,0 +1,27 @@
+-- CREATE DATABASE Company;
+
+-- USE Company;
+
+-- CREATE TABLE Employees (
+-- 	EmployeeID INT PRIMARY KEY,
+--     FirstName VARCHAR(20),
+--     LastName VARCHAR(20),
+--     Age INT,0
+--     Department VARCHAR(1000)
+--     );
+
+-- INSERT INTO Employees (EmployeeID, FirstName, LastName, Age, Department)
+-- VALUE 
+-- 	(1, "Andrey" , "Garcia" , 20 , "Data Privacy"),
+--     (2, "Daniel" , "Teves" , 21 , "Office of the Student Affairs and Services"),
+--     (3, "Jericho" , "Borromeo" , 22 , "Library"),
+--     (4, "Angelo" , "Delos Santos" , 21 , "Library"),
+--     (5, "Peter" , "Factor" , 21 , "Physical Education");
+
+-- SELECT * FROM Employees;
+
+-- UPDATE Employees SET Department = "Marketing" WHERE EmployeeID = 2;
+
+-- DELETE FROM Employees WHERE EmployeeID = 3;
+
+-- DROP TABLE Employees;
